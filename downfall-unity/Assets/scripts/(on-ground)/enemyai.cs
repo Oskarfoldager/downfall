30c0f1632e784dbdbbcaf50793d3a8d0c22ff3b7
oskar ,ok <foldagerfamilie@gmail.com>
2022-11-12 15:43:05 +0100

    new scene

---
+++ downfall-unity/Assets/scripts/(on-ground)/enemyai.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyai : MonoBehaviour
+{
+    public Transform player;
+    public Transform self;
+    public float speed;
+    public float lengthbetween;
+    public float radiusofattack;
+    public float attackspeed = 20f;
+    private float nextactiontime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movetowardsplayer();
+        lengthbetween = Mathf.Sqrt((player.position.x - self.position.x) * (player.position.x - self.position.x));
+    }
+    void movetowardsplayer()
+    {
+        if (lengthbetween < radiusofattack)
+        {
+            attackplayer();
+        }
+        else
+        {
+            self.position = Vector3.MoveTowards(self.position, player.position, speed);
+        }
+    }
+    void attackplayer()
+    {
+        if (Time.time > nextactiontime)
+        {
+
+            nextactiontime += period;
+        }
+    }
+}
