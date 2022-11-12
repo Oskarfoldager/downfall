9de6bd41480a3022e374742b2ba5bf443fbface6
thom234u <thomas@fage-butler.dk>
2022-11-12 19:33:24 +0100

    mainmenu

---
+++ downfall-unity/Assets/scripts/switchtolevels.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class switchtolevels : MonoBehaviour
+{
+    public void nu()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
