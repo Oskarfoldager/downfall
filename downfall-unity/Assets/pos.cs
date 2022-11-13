be881d98fec4b43bae102effa0aa2aaffb84f0fc
Adam Dalsgaard Andersen <adamdalsgaard@gmail.com>
2022-11-13 10:11:23 +0100

    yesyes

--- downfall-unity/Assets/pos.cs
+++ downfall-unity/Assets/pos.cs
@@ -5,27 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class pos : MonoBehaviour
 {
-    public GameObject Flyver;
-    public float speed;
-    public bool bothit;
-    // Start is called before the first frame update
-    void Start()
-    {
-        bothit = false;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (bothit == true)
-        {
-            SceneManager.LoadScene(0);
-        }
-    }
     public void OnTriggerEnter2D(Collider2D col)
     {
 
-        bothit = true;
+        SceneManager.LoadScene(7);
         //chair.transform.Translate (Vector3.right* speed * Time.deltaTime);
     }
 
